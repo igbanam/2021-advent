f57a0c8e154a80e5d930c604c890ab582ab1d924
Owajigbanam Ogbuluijah ✊🏾 <xigbanam@gmail.com>
2021-12-13 12:37:31 +0000

    Day 1 - Part 2 (Copilot wins!!!)

--- 1.rb
+++ 1.rb
@@ -2,8 +2,17 @@
 
 # Day 1: Sonar Sweep
 
-filename = $0.gsub(".rb", "")
+# Sum every three inputs in array
+def sum_three(input)
+  input.each_cons(3).map { |a, b, c| a + b + c }
+end
+
+def sonar_sweep(input)
+  increments = 0
+  input.each_cons(2) { |p, n| increments += 1 if p < n }
+  increments
+end
+
+filename = $PROGRAM_NAME.gsub('.rb', '')
 depthmap = File.readlines("./#{filename}.in", chomp: true).map!(&:to_i)
-increments = 0
-depthmap.each_cons(2) { |p, n| increments += 1 if p < n }
-puts increments
+puts sonar_sweep(sum_three(depthmap))
