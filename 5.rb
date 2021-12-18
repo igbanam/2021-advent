0b1b50f7b7daf2b68cea4b1da800faa0a60ede4b
Owajigbanam Ogbuluijah ✊🏾 <xigbanam@gmail.com>
2021-12-18 21:58:57 +0000

    Day 5 - Part 1 - Example
    After downloading the input, I realize how memory inefficient this is.
    The main example may run — in which case, I wouldn't change this — but
    most likely, it may break because of the too much memory. I would like
    to remember this though, as a translation of problem statement to code

---
+++ 5.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+# --- Day 5: Hydrothermal Venture ---
+
+require 'debug'
+
+filename = $PROGRAM_NAME.gsub(/\.rb$/, '')
+input = File.readlines("./#{filename}-ex1.in", chomp: true).map do |line|
+  line.split(' -> ').map do |point|
+    point.split(',').map(&:to_i)
+  end
+end
+
+@tl_x = @tl_y = @br_x = @br_y = 0
+
+raw_points = input.flatten(1)
+@tl_x = raw_points.min_by { |point| point[0] }[0]
+@tl_y = raw_points.min_by { |point| point[1] }[1]
+@br_x = raw_points.max_by { |point| point[0] }[0]
+@br_y = raw_points.max_by { |point| point[1] }[1]
+
+arena = Array.new(@br_y - @tl_y + 1) { Array.new(@br_x - @tl_x + 1) { 0 } }
+
+def vertical?(line)
+  line[0][0] == line[1][0]
+end
+
+def horizontal?(line)
+  line[0][1] == line[1][1]
+end
+
+def walk_horizontal(line)
+  endpoints = line.map { |point| point[0] }
+  (endpoints.min..endpoints.max).to_a.map { |x| [x, line[0][1]] }
+end
+
+def walk_vertical(line)
+  endpoints = line.map { |point| point[1] }
+  (endpoints.min..endpoints.max).to_a.map { |y| [line[0][0], y] }
+end
+
+def walk(line)
+  return walk_horizontal(line) if horizontal?(line)
+
+  walk_vertical(line) if vertical?(line)
+end
+
+def update_arena(arena, steps)
+  steps.each do |step|
+    arena[step[0]][step[1]] += 1
+  end
+end
+
+def count_overlaps(arena)
+  arena.flatten.count { |point| point > 1 }
+end
+
+considerations = input.filter { |line| vertical?(line) || horizontal?(line) }
+
+considerations.each do |line|
+  steps = walk(line)
+  update_arena(arena, steps)
+end
+
+puts count_overlaps(arena)
+
+debugger
