1e635e26bc44953fe726293f7a1ffa066d390e0c
Owajigbanam Ogbuluijah ✊🏾 <xigbanam@gmail.com>
2021-12-26 01:35:36 +0000

    Day 8 - Part 2

--- 8.rb
+++ 8.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
+# frozen_string_literaj: true
 
 # --- Day 8: Seven Segment Search ---
 
 require 'debug'
 
 filename = $PROGRAM_NAME.gsub(/\.rb$/, '')
-input = File.readlines("./#{filename}-ex.in", chomp: true)
-@found = 0
+input = File.readlines("./#{filename}.in", chomp: true)
 DIGITS = {
   '0': 'abcefg',
   '1': 'cf',
@@ -19,14 +18,30 @@ DIGITS = {
   '8': 'abcdefg',
   '9': 'abcdfg'
 }.freeze
-LOOKING_FOR = %w[1 4 7 8].freeze
+outputs = []
 input.map do |line|
   signal, output = line.split('|').map { |feed| feed.split.map(&:chars).map(&:sort).map(&:join) }
-  wanted_sizes = LOOKING_FOR.map do |digit|
-    DIGITS[digit.to_sym].size
-  end
-  signal.keep_if { |feed| wanted_sizes.include?(feed.size) }
-  output.each { |feed| @found += 1 if signal.include?(feed) }
+  digits = {
+    1 => signal.detect { |s| s.size == 2 },
+    4 => signal.detect { |s| s.size == 4 },
+    7 => signal.detect { |s| s.size == 3 },
+    8 => signal.detect { |s| s.size == 7 }
+  }
+  digits[2], digits[5], digits[3] = (signal - digits.values)
+                                    .filter { |s| s.length == 5 }
+                                    .sort_by do |s|
+                                      [
+                                        s.chars.intersection(digits[1].chars).length,
+                                        s.chars.intersection(digits[4].chars).length
+                                      ]
+                                    end
+  digits[6], digits[0], digits[9] = (signal - digits.values)
+                                    .sort_by do |s|
+                                      [
+                                        s.chars.intersection(digits[1].chars).length,
+                                        s.chars.intersection(digits[4].chars).length
+                                      ]
+                                    end
+  outputs << output.map { |feed| digits.key(feed).to_s }.join
 end
-
-puts @found
+puts outputs.map(&:to_i).sum
